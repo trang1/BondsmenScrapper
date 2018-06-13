03bd814709cfc49770962da88ee2bb8cea14a14d
trang1 <trang@bk.ru>
2018-06-13 18:54:19 +0300

    Form design
    Initial actions

---
+++ src/BondsmenScrapper/MainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace BondsmenScrapper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbBondsman = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbBondsman);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbPassword);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbUsername);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(427, 153);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Parameters";
+            // 
+            // tbBondsman
+            // 
+            this.tbBondsman.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbBondsman.Location = new System.Drawing.Point(106, 109);
+            this.tbBondsman.Name = "tbBondsman";
+            this.tbBondsman.Size = new System.Drawing.Size(315, 23);
+            this.tbBondsman.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(13, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Bondsman #";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbPassword.Location = new System.Drawing.Point(106, 72);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(315, 23);
+            this.tbPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(31, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(27, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tbUsername.Location = new System.Drawing.Point(106, 38);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(315, 23);
+            this.tbUsername.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(99, 172);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(180, 172);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tbLog
+            // 
+            this.tbLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.tbLog.Location = new System.Drawing.Point(3, 16);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.Size = new System.Drawing.Size(422, 331);
+            this.tbLog.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbLog);
+            this.groupBox2.Location = new System.Drawing.Point(12, 201);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(428, 350);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 563);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Bondsmen Scrapper";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbBondsman;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ src/BondsmenScrapper/MainForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace BondsmenScrapper
+{
+    public partial class MainForm : Form
+    {
+        private volatile bool _started;
+        private static readonly HttpClient client = new HttpClient();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = true;
+            btnStart.Enabled = false;
+
+            Task.Factory.StartNew(BeginScrapping);
+            _started = true;
+            Log("Starting...");
+        }
+
+        private void BeginScrapping()
+        {
+            var loginUrl = ConfigurationManager.AppSettings.Get("LoginUrl");
+            var proxy = ConfigurationManager.AppSettings.Get("ProxyIP");
+            int port = -1;
+            int.TryParse(ConfigurationManager.AppSettings.Get("ProxyPort"), out port);
+
+            WebProxy webProxy = null;
+
+            if (!string.IsNullOrEmpty(proxy) && port != -1)
+            {
+                webProxy = new WebProxy(proxy, port);    
+            }
+            
+            var web = new HtmlWeb();
+            HtmlDocument htmlDoc;
+
+            if (webProxy != null)
+                htmlDoc = web.Load(loginUrl, "GET", webProxy, null);
+            else
+                htmlDoc = web.Load(loginUrl);
+
+            var values = new Dictionary<string, string>
+            {
+                {"_EVENTTARGET", ""},
+                {"_EVENTARGUMENT", ""},
+                {"_VIEWSTATE", "wEPDwUKLTY0NDEwNzIzNw9kFgICAQ8WAh4GYWN0aW9uBVMvZURvY3MvU2VjdXJlL1dpZGVMb2dpbi5hc3B4P1JldHVyblVybD0lMmZFZG9jcyUyZlB1YmxpYyUyZnNlYXJjaC5hc3B4JTNmU2hvd0ZGJTNkMRYEAgEPZBYEAhMPDxYCHgdWaXNpYmxlaGRkAhUPDxYEHgtOYXZpZ2F0ZVVybAVTL2VEb2NzL1NlY3VyZS9XaWRlTG9naW4uYXNweD9SZXR1cm5Vcmw9JTJmRWRvY3MlMmZQdWJsaWMlMmZzZWFyY2guYXNweCUzZlNob3dGRiUzZDEfAWhkZAIDDw8WAh8BaGRkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBQZidG5GQVGkUD5djGE0z9dz99IDxlw5D1"},
+                {"_VIEWSTATEGENERATOR", "2728C45E"},
+                {"_EVENTVALIDATION", @"/wEdAAfeViMp48uSrjgWll44z7nVY3plgk0YBAefRz3MyBlTcHY2+Mc6SrnAqio3oCKbxYZXqn8G/9ILxNg5awJtLjbUlbQysvvHYo6UvZRsI/lgzil0qZQk33egat5tMnM3Tn7JNU05kLxUP1MfqDgOBl2VhEhbV4SgucMtGzWzShpD+lrIIWg="},
+                {"txtUserName", "1"},
+                {"txtPassword", "1"},
+                {"btnLoginImageButton", "Login" }
+            };
+
+            //var content = new FormUrlEncodedContent(values);
+
+            //var response = client.PostAsync(@"https://www.hcdistrictclerk.com/eDocs/Secure/WideLogin.aspx?ReturnUrl=%2fEdocs%2fPublic%2fsearch.aspx%3fShowFF%3d1", content);
+           // var responseString = response.Content.ReadAsStringAsync();
+        }
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStop.Enabled = false;
+            btnStart.Enabled = true;
+
+            _started = false;
+            Log("Stopping...");
+        }
+
+        private void Log(string message)
+        {
+            tbLog.Text += $"{DateTime.Now:HH:mm:ss}: {message}\r\n";
+
+            if (tbLog.Lines.Length > 10)
+            {
+                var file = Path.Combine(Application.StartupPath, $"{DateTime.Now:yyyy-MM-dd_HH-mm} .log");
+                File.WriteAllText(file, tbLog.Text);
+
+                tbLog.Text = string.Empty;
+                Log("Log flushed. All data saved to " + file);
+            }
+        }
+    }
+}
