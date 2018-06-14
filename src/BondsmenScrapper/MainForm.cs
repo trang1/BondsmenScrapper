2656aa02053a5186b592977c0ad78752aec6d519
trang1 <trang@bk.ru>
2018-06-15 00:26:40 +0300

    Login

--- src/BondsmenScrapper/MainForm.cs
+++ src/BondsmenScrapper/MainForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
@@ -9,6 +10,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using HtmlAgilityPack;
@@ -19,11 +21,16 @@ namespace BondsmenScrapper
     public partial class MainForm : Form
     {
         private volatile bool _started;
-        private static readonly HttpClient client = new HttpClient();
+        private WebClient _client;
+        private WebProxy _proxy;
+        public string _cookie;
 
         public MainForm()
         {
             InitializeComponent();
+            _client = new GZipWebClient();
+            tbUsername.Text = "nbaca@walden.ly";
+            tbPassword.Text = "1.Upwork";
         }
 
         private void btnStart_Click(object sender, EventArgs e)
@@ -39,45 +46,109 @@ namespace BondsmenScrapper
         private void BeginScrapping()
         {
             var loginUrl = ConfigurationManager.AppSettings.Get("LoginUrl");
+            var url = ConfigurationManager.AppSettings.Get("Url");
             var proxy = ConfigurationManager.AppSettings.Get("ProxyIP");
             int port = -1;
             int.TryParse(ConfigurationManager.AppSettings.Get("ProxyPort"), out port);
-
-            WebProxy webProxy = null;
-
+            
             if (!string.IsNullOrEmpty(proxy) && port != -1)
             {
-                webProxy = new WebProxy(proxy, port);    
+                _proxy = new WebProxy(proxy, port);
+                _client.Proxy = _proxy;
+                Log($"Using proxy {proxy}:{port}");
             }
             
             var web = new HtmlWeb();
             HtmlDocument htmlDoc;
-
-            if (webProxy != null)
-                htmlDoc = web.Load(loginUrl, "GET", webProxy, null);
+            web.PostResponse = delegate(HttpWebRequest request, HttpWebResponse response)
+            {
+                var cookies = response.Headers["Set-Cookie"];
+                var sessionId = cookies.Split(';')[0];
+                _client.Headers.Add(HttpRequestHeader.Cookie, sessionId + "; Login=");
+            };
+            Log("Connecting to the website");
+            if (_proxy != null)
+                htmlDoc = web.Load(loginUrl, "GET", _proxy, null);
             else
                 htmlDoc = web.Load(loginUrl);
-
-            var values = new Dictionary<string, string>
+            Thread.Sleep(2000);
+            Log("Trying to login");
+            if (Login(htmlDoc, loginUrl))
             {
-                {"_EVENTTARGET", ""},
-                {"_EVENTARGUMENT", ""},
-                {"_VIEWSTATE", "wEPDwUKLTY0NDEwNzIzNw9kFgICAQ8WAh4GYWN0aW9uBVMvZURvY3MvU2VjdXJlL1dpZGVMb2dpbi5hc3B4P1JldHVyblVybD0lMmZFZG9jcyUyZlB1YmxpYyUyZnNlYXJjaC5hc3B4JTNmU2hvd0ZGJTNkMRYEAgEPZBYEAhMPDxYCHgdWaXNpYmxlaGRkAhUPDxYEHgtOYXZpZ2F0ZVVybAVTL2VEb2NzL1NlY3VyZS9XaWRlTG9naW4uYXNweD9SZXR1cm5Vcmw9JTJmRWRvY3MlMmZQdWJsaWMlMmZzZWFyY2guYXNweCUzZlNob3dGRiUzZDEfAWhkZAIDDw8WAh8BaGRkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBQZidG5GQVGkUD5djGE0z9dz99IDxlw5D1"},
-                {"_VIEWSTATEGENERATOR", "2728C45E"},
-                {"_EVENTVALIDATION", @"/wEdAAfeViMp48uSrjgWll44z7nVY3plgk0YBAefRz3MyBlTcHY2+Mc6SrnAqio3oCKbxYZXqn8G/9ILxNg5awJtLjbUlbQysvvHYo6UvZRsI/lgzil0qZQk33egat5tMnM3Tn7JNU05kLxUP1MfqDgOBl2VhEhbV4SgucMtGzWzShpD+lrIIWg="},
-                {"txtUserName", "1"},
-                {"txtPassword", "1"},
-                {"btnLoginImageButton", "Login" }
+                Log("Login successful");
+
+                web = new HtmlWeb();
+                web.PreRequest = delegate (HttpWebRequest request)
+                {
+                    request.Headers["Cookie"] = _cookie;
+                    return true;
+                };
+                htmlDoc = web.Load(url, "GET", _proxy, null);
+            }
+            else
+            {
+                Log("Login failed");
+                BeginInvoke(new Action(Stop));
+            }
+        }
+
+        private bool Login(HtmlDocument htmlDoc, string loginUrl)
+        {
+            var eventTarget =
+                htmlDoc.DocumentNode.SelectSingleNode("//input[@name='__EVENTTARGET']").GetAttributeValue("value", "");
+
+            var eventArgument =
+                htmlDoc.DocumentNode.SelectSingleNode("//input[@name='__EVENTARGUMENT']").GetAttributeValue("value", "");
+            var viewState =
+                htmlDoc.DocumentNode.SelectSingleNode("//input[@name='__VIEWSTATE']").GetAttributeValue("value", "");
+            var viewStateGenerator =
+                htmlDoc.DocumentNode.SelectSingleNode("//input[@name='__VIEWSTATEGENERATOR']").GetAttributeValue("value", "");
+            var eventValidation =
+                htmlDoc.DocumentNode.SelectSingleNode("//input[@name='__EVENTVALIDATION']").GetAttributeValue("value", "");
+            var btnLoginValue =
+                htmlDoc.DocumentNode.SelectSingleNode("//input[@name='btnLoginImageButton']").GetAttributeValue("value", "");
+
+            var values = new NameValueCollection()
+            {
+                {"__EVENTTARGET", eventTarget},
+                {"__EVENTARGUMENT", eventArgument},
+                {"__VIEWSTATE", viewState},
+                {"__VIEWSTATEGENERATOR", viewStateGenerator},
+                {"__EVENTVALIDATION", eventValidation},
+                {"txtUserName", tbUsername.Text},
+                {"txtPassword", tbPassword.Text},
+                {"btnLoginImageButton", btnLoginValue}
             };
 
-            //var content = new FormUrlEncodedContent(values);
+            //_client.Headers.Add(HttpRequestHeader.Connection,"keep-alive");
+            _client.Headers.Add(HttpRequestHeader.ContentType, "application/x-www-form-urlencoded");
+            _client.Headers.Add(HttpRequestHeader.Host, "www.hcdistrictclerk.com");
+           // _client.Headers.Add("Connection", "keep-alive");
+            _client.Headers.Add("DNT", "1");
+            _client.Headers.Add(HttpRequestHeader.Referer, loginUrl);
+            _client.Headers.Add("Upgrade-Insecure-Requests", "1");
+            _client.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0");
+            _client.Headers.Add(HttpRequestHeader.KeepAlive,"");
+            _client.Headers.Add(HttpRequestHeader.Accept, "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            _client.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate, br");
+            _client.Headers.Add(HttpRequestHeader.AcceptLanguage, "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
+            // var content = new FormUrlEncodedContent(values);
 
-            //var response = client.PostAsync(@"https://www.hcdistrictclerk.com/eDocs/Secure/WideLogin.aspx?ReturnUrl=%2fEdocs%2fPublic%2fsearch.aspx%3fShowFF%3d1", content);
-           // var responseString = response.Content.ReadAsStringAsync();
+            var result = Encoding.ASCII.GetString(_client.UploadValues(loginUrl, values));
+            _cookie = _client.ResponseHeaders["Set-Cookie"];
+
+            return _cookie.Length > 500;
+            //var response = _client.PostAsync(loginUrl, content).GetAwaiter().GetResult();
+            //var result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
         }
 
 
         private void btnStop_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void Stop()
         {
             btnStop.Enabled = false;
             btnStart.Enabled = true;
@@ -88,16 +159,32 @@ namespace BondsmenScrapper
 
         private void Log(string message)
         {
-            tbLog.Text += $"{DateTime.Now:HH:mm:ss}: {message}\r\n";
+            BeginInvoke(new Action(() => tbLog.Text += $"{DateTime.Now:HH:mm:ss}: {message}\r\n"));
 
             if (tbLog.Lines.Length > 10)
             {
                 var file = Path.Combine(Application.StartupPath, $"{DateTime.Now:yyyy-MM-dd_HH-mm} .log");
                 File.WriteAllText(file, tbLog.Text);
 
-                tbLog.Text = string.Empty;
+                BeginInvoke(new Action(() => tbLog.Text = string.Empty));
                 Log("Log flushed. All data saved to " + file);
             }
+        }
+    }
+
+    public class GZipWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            HttpWebRequest request = (HttpWebRequest)base.GetWebRequest(address);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            return request;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request)
+        {
+            var webResponse = base.GetWebResponse(request);
+            return webResponse;
         }
     }
 }
