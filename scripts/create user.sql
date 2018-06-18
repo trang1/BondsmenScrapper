63c3c6a0bf0c22d1329b9acfdbc36566b35db012
trang1 <trang@bk.ru>
2018-06-18 23:18:11 +0300

    Final scripts

--- scripts/create user.sql
+++ scripts/create user.sql
@@ -1,3 +1,3 @@
-CREATE USER 'bnd'@'localhost';
-SET PASSWORD FOR 'bnd'@'localhost' = PASSWORD('bnd');
-GRANT Select, Delete, Insert, Trigger, Event, Execute, Alter Routine, Create Routine, Show View, Create View, Lock Tables, Create Temporary Tables, Alter, Index, References, Grant Option, Drop, Create, Update ON bondsman.* TO 'bnd'@'localhost';
+CREATE USER 'bond'@'localhost';
+SET PASSWORD FOR 'bond'@'localhost' = PASSWORD('bond1');
+GRANT Select, Delete, Insert, Trigger, Event, Execute, Alter Routine, Create Routine, Show View, Create View, Lock Tables, Create Temporary Tables, Alter, Index, References, Grant Option, Drop, Create, Update ON bondsmen.* TO 'bond'@'localhost';
